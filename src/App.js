48eefc3fad921a72af799269cde2d64691f2c072
Phoenix <drake.mcgarrah@gmail.com>
2020-08-14 10:03:20 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from 'react';
+import Posts from "./pages/Posts";
+import Error from "./pages/Error";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/"
+          render={props => (<Posts {...props} />)} />
+        <Route exact path="/error"
+          render={props => (<Error {...props} />)} />
+        <Route exact path="/error/comment"
+          render={props => (<Error {...props} specific={"comment"} />)} />
+        <Route exact path="/error/characters"
+          render={props => (<Error {...props} specific={"characters"} />)} />
+      </div >
+    </Router>
   );
 }
 
