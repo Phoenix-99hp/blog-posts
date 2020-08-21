1c178d5cd1ff43476554abc885a03b90764e4bd1
Phoenix <drake.mcgarrah@gmail.com>
2020-08-21 11:55:37 -0500

    Using hash router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Posts from "./pages/Posts";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Error from "./pages/Error";
 
 function App() {
